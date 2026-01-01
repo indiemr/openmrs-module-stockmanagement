bc2aecc552fe571657d194ee59fbb98ad41e22b0
ganeshdevisetti <jayendraganesh21@gmail.com>
2026-01-01 11:49:16 +0530

    Revert "TRUNK-6336: Adjust stockmanagement module to Hibernate Search 6.2 (#41)"
    This reverts commit 0a39cefaff85fcfc889c4a287494076a501d5acf.
    
    Reverting changes of upgrading hibernate

--- api/src/main/java/org/openmrs/module/stockmanagement/api/jobs/StockItemImportJob.java
+++ api/src/main/java/org/openmrs/module/stockmanagement/api/jobs/StockItemImportJob.java
@@ -1,12 +1,6 @@
 package org.openmrs.module.stockmanagement.api.jobs;
 
-import com.opencsv.CSVReader;
-import com.opencsv.CSVReaderBaseBuilder;
-import com.opencsv.CSVReaderBuilder;
-import com.opencsv.CSVReaderHeaderAware;
-import com.opencsv.CSVReaderHeaderAwareBuilder;
-import com.opencsv.RFC4180Parser;
-import com.opencsv.RFC4180ParserBuilder;
+import liquibase.util.csv.CSVReader;
 import org.apache.commons.lang.StringUtils;
 import org.openmrs.Concept;
 import org.openmrs.Drug;
@@ -822,12 +816,7 @@ public class StockItemImportJob {
             try(Writer writer = Files.newBufferedWriter( new File(file.toString() + "_errors").toPath())) {
                 boolean resetErrors = false;
                 try (Reader reader = Files.newBufferedReader(file)) {
-                    RFC4180Parser parser = new RFC4180ParserBuilder().withSeparator(',').withQuoteChar('\"').build();
-                    if (hasHeader) {
-                        csvReader = new CSVReaderHeaderAwareBuilder(reader).withCSVParser(parser).build();
-                    } else {
-                        csvReader = new CSVReaderBuilder(reader).withCSVParser(parser).build();
-                    }
+                    csvReader = new CSVReader(reader);
                     String[] csvLine = null;
                     boolean processedPending = false;
                     Map<Integer, Object[]> list = new HashMap<>();
